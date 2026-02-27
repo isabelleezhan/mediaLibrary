0dfc9c4902f6bc180dc493cf2563016a135b429e
Isabelle Zhan <izhan@students.cs.ubc.ca>
2026-02-27 15:05:18 -0800

    Added tests for JsonReader

---
+++ src/test/persistence/JsonReaderTest.java
@@ -0,0 +1,61 @@
+package persistence;
+
+import model.*;
+
+import static org.junit.jupiter.api.Assertions.*;
+import org.junit.jupiter.api.Test;
+
+import java.io.IOException;
+import java.util.List;
+
+import ca.ubc.cs.ExcludeFromJacocoGeneratedReport;
+
+@ExcludeFromJacocoGeneratedReport
+public class JsonReaderTest extends JsonTest {
+
+    private JsonReader reader;
+
+    @Test
+    void testConstructor() {
+        reader = new JsonReader("./data/mediaLibrary.json");
+        assertEquals("./data/mediaLibrary.json", reader.getSource());
+    }
+    
+    @Test
+    void testReaderNoFile() {
+        reader = new JsonReader("./data/NA.json");
+        try {
+            reader.read();
+            fail("Should've thrown IOException");
+        } catch (IOException e) {
+        }
+    }
+
+    @Test
+    void testReaderEmpty() {
+        reader = new JsonReader("./data/empty.json");
+        try {
+            MediaLibrary ml = reader.read();
+            assertEquals(0, ml.getAllMedia().size());
+        } catch (IOException e) {
+            fail("Shouldn't throw an exception");
+        }
+    }
+
+    @Test
+    void testReaderGeneralLibrary() {
+        reader = new JsonReader("./data/generalLibrary.json");
+        try {
+            MediaLibrary ml = reader.read();
+            List<Media> lib = ml.getAllMedia();
+            assertEquals(3, lib.size());
+            checkMovie((Movie) lib.get(0), "Hamnet", "Drama", Status.FINISHED, "Chloe Zhao");
+            checkRatingAndReview(lib.get(0), "It was good!", 4);
+            checkTVShow((TVShow) lib.get(1), "Game of Thrones", "Fantasy", Status.FINISHED, 8);
+            checkRatingAndReview(lib.get(1), "If only they stopped at season 4...", 5);
+            checkBook((Book) lib.get(2), "Babel", "Historical Fantasy", Status.IN_PROGRESS, "R.F. Kuang");
+        } catch (IOException e) {
+            fail("Couldn't read from file");
+        }
+    }
+}
---
+++ src/test/persistence/JsonTest.java
@@ -0,0 +1,37 @@
+package persistence;
+
+import model.*;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+import ca.ubc.cs.ExcludeFromJacocoGeneratedReport;
+
+@ExcludeFromJacocoGeneratedReport
+public class JsonTest {
+
+    protected void checkMedia(Media media, String title, String genre, Status status) {
+        assertEquals(title, media.getTitle());
+        assertEquals(genre, media.getGenre());
+        assertEquals(status, media.getStatus());
+    }
+
+    protected void checkRatingAndReview(Media media, String review, int rating) {
+        assertEquals(review, media.getReview());
+        assertEquals(rating, media.getRating());
+    }
+
+    protected void checkBook(Book book, String title, String genre, Status status, String author) {
+        checkMedia(book, title, genre, status);
+        assertEquals(author, book.getAuthor());
+    }
+
+    protected void checkMovie(Movie movie, String title, String genre, Status status, String director) {
+        checkMedia(movie, title, genre, status);
+        assertEquals(director, movie.getDirector());
+    }
+
+    protected void checkTVShow(TVShow show, String title, String genre, Status status, int numSeasons) {
+        checkMedia(show, title, genre, status);
+        assertEquals(numSeasons, show.getNumSeasons());
+    }
+}
